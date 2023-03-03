0d2315fd195818d039258862163b6775b95b977c
sucheendraram <sucheendraramesh93@gmail.com>
2023-03-03 22:26:04 +0530

    Driver changed to Firefox

--- src/com/datadriven/sourcecode/DataDrivenApproachExample.java
+++ src/com/datadriven/sourcecode/DataDrivenApproachExample.java
@@ -6,14 +6,14 @@ import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class DataDrivenApproachExample {
 
 	public static void main(String[] args) {
 		try {
-			ChromeDriver driver = new ChromeDriver();
+			FirefoxDriver driver = new FirefoxDriver();
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			FileInputStream fis = new FileInputStream(
